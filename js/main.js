528b808346965c3ccd2a02d4762347847b2b409a
Oksana Morozova <om36134@gmail.com>
2020-03-12 15:20:29 +0300

    comment

---
+++ js/main.js
@@ -0,0 +1,177 @@
+var res="231134224413313341242123313";
+function check_me()
+{
+    var count=0;
+    with(document.test) {
+        if (!Q1[0].checked&&!Q1[1].checked&&!Q1[2].checked&&!Q1[3].checked)
+        {count+=1};
+        if (!Q2[0].checked&&!Q2[1].checked&&!Q2[2].checked&&!Q2[3].checked)
+        {count+=1};
+        if (!Q3[0].checked&&!Q3[1].checked&&!Q3[2].checked&&!Q3[3].checked)
+        {count+=1};
+        if (!Q4[0].checked&&!Q4[1].checked&&!Q4[2].checked&&!Q4[3].checked)
+        {count+=1};
+        if (!Q5[0].checked&&!Q5[1].checked&&!Q5[2].checked&&!Q5[3].checked)
+        {count+=1};
+        if (!Q6[0].checked&&!Q6[1].checked&&!Q6[2].checked&&!Q6[3].checked)
+        {count+=1};
+        if (!Q7[0].checked&&!Q7[1].checked&&!Q7[2].checked&&!Q7[3].checked)
+        {count+=1};
+        if (!Q8[0].checked&&!Q8[1].checked&&!Q8[2].checked&&!Q8[3].checked)
+        {count+=1};
+        if (!Q9[0].checked&&!Q9[1].checked&&!Q9[2].checked&&!Q9[3].checked)
+        {count+=1};
+        if (!Q10[0].checked&&!Q10[1].checked&&!Q10[2].checked&&!Q10[3].checked)
+        {count+=1};
+        if (!Q11[0].checked&&!Q11[1].checked&&!Q11[2].checked&&!Q11[3].checked)
+        {count+=1};
+        if (!Q12[0].checked&&!Q12[1].checked&&!Q12[2].checked&&!Q12[3].checked)
+        {count+=1};
+        if (!Q13[0].checked&&!Q13[1].checked&&!Q13[2].checked&&!Q13[3].checked)
+        {count+=1};
+        if (!Q14[0].checked&&!Q14[1].checked&&!Q14[2].checked&&!Q14[3].checked)
+        {count+=1};
+        if (!Q15[0].checked&&!Q15[1].checked&&!Q15[2].checked&&!Q15[3].checked)
+        {count+=1};
+        if (!Q16[0].checked&&!Q16[1].checked&&!Q16[2].checked&&!Q16[3].checked)
+        {count+=1};
+        if (!Q17[0].checked&&!Q17[1].checked&&!Q17[2].checked&&!Q17[3].checked)
+        {count+=1};
+        if (!Q18[0].checked&&!Q18[1].checked&&!Q18[2].checked&&!Q18[3].checked)
+        {count+=1};
+        if (!Q19[0].checked&&!Q19[1].checked&&!Q19[2].checked&&!Q19[3].checked)
+        {count+=1};
+        if (!Q20[0].checked&&!Q20[1].checked&&!Q20[2].checked&&!Q20[3].checked)
+        {count+=1};
+        if (!Q21[0].checked&&!Q21[1].checked&&!Q21[2].checked&&!Q21[3].checked)
+        {count+=1};
+        if (!Q22[0].checked&&!Q22[1].checked&&!Q22[2].checked&&!Q22[3].checked)
+        {count+=1};
+        if (!Q23[0].checked&&!Q23[1].checked&&!Q23[2].checked&&!Q23[3].checked)
+        {count+=1};
+        if (!Q24[0].checked&&!Q24[1].checked&&!Q24[2].checked&&!Q24[3].checked)
+        {count+=1};
+        if (!Q25[0].checked&&!Q25[1].checked&&!Q25[2].checked&&!Q25[3].checked)
+        {count+=1};
+        if (!Q26[0].checked&&!Q26[1].checked&&!Q26[2].checked&&!Q26[3].checked)
+        {count+=1};
+        if (!Q27[0].checked&&!Q27[1].checked&&!Q27[2].checked&&!Q27[3].checked)
+        {count+=1};
+        if (count>0) alert("Вы выполнили не все задания. Проверьте себя!")
+        else answer();
+    }
+}
+
+function control(k, f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17,f18,f19,f20,f21,f22,f23,f24,f25,f26,f27) {
+    if (k==1&&f1.checked) return true;
+    if (k==2&&f2.checked) return true;
+    if (k==3&&f3.checked) return true;
+    if (k==4&&f4.checked) return true;
+    if (k==5&&f5.checked) return true;
+    if (k==6&&f6.checked) return true;
+    if (k==7&&f7.checked) return true;
+    if (k==8&&f8.checked) return true;
+    if (k==9&&f9.checked) return true;
+    if (k==10&&f10.checked) return true;
+    if (k==11&&f11.checked) return true;
+    if (k==12&&f12.checked) return true;
+    if (k==13&&f13.checked) return true;
+    if (k==14&&f14.checked) return true;
+    if (k==15&&f15.checked) return true;
+    if (k==16&&f16.checked) return true;
+    if (k==17&&f17.checked) return true;
+    if (k==18&&f18.checked) return true;
+    if (k==19&&f19.checked) return true;
+    if (k==20&&f20.checked) return true;
+    if (k==21&&f21.checked) return true;
+    if (k==22&&f22.checked) return true;
+    if (k==23&&f23.checked) return true;
+    if (k==24&&f24.checked) return true;
+    if (k==25&&f25.checked) return true;
+    if (k==26&&f26.checked) return true;
+    if (k==27&&f27.checked) return true;
+    return false;
+}
+
+function answer() {
+    answ="";
+    with(document)    {
+        answ+=control(res.charAt(0) ,test.Q1[0],test.Q1[1],test.Q1[2],test.Q1[3])?"1":"0";
+        answ+=control(res.charAt(1) ,test.Q2[0],test.Q2[1],test.Q2[2],test.Q2[3])?"1":"0";
+        answ+=control(res.charAt(2) ,test.Q3[0],test.Q3[1],test.Q3[2],test.Q3[3])?"1":"0";
+        answ+=control(res.charAt(3) ,test.Q4[0],test.Q4[1],test.Q4[2],test.Q4[3])?"1":"0";
+        answ+=control(res.charAt(4) ,test.Q5[0],test.Q5[1],test.Q5[2],test.Q5[3])?"1":"0";
+        answ+=control(res.charAt(5) ,test.Q6[0],test.Q6[1],test.Q6[2],test.Q6[3])?"1":"0";
+        answ+=control(res.charAt(6) ,test.Q7[0],test.Q7[1],test.Q7[2],test.Q7[3])?"1":"0";
+        answ+=control(res.charAt(7) ,test.Q8[0],test.Q8[1],test.Q8[2],test.Q8[3])?"1":"0";
+        answ+=control(res.charAt(8) ,test.Q9[0],test.Q9[1],test.Q9[2],test.Q9[3])?"1":"0";
+        answ+=control(res.charAt(9) ,test.Q10[0],test.Q10[1],test.Q10[2],test.Q10[3])?"1":"0";
+        answ+=control(res.charAt(10) ,test.Q11[0],test.Q11[1],test.Q11[2],test.Q11[3])?"1":"0";
+        answ+=control(res.charAt(11) ,test.Q12[0],test.Q12[1],test.Q12[2],test.Q12[3])?"1":"0";
+        answ+=control(res.charAt(12) ,test.Q13[0],test.Q13[1],test.Q13[2],test.Q13[3])?"1":"0";
+        answ+=control(res.charAt(13) ,test.Q14[0],test.Q14[1],test.Q14[2],test.Q14[3])?"1":"0";
+        answ+=control(res.charAt(14) ,test.Q15[0],test.Q15[1],test.Q15[2],test.Q15[3])?"1":"0";
+        answ+=control(res.charAt(15) ,test.Q16[0],test.Q16[1],test.Q16[2],test.Q16[3])?"1":"0";
+        answ+=control(res.charAt(16) ,test.Q17[0],test.Q17[1],test.Q17[2],test.Q17[3])?"1":"0";
+        answ+=control(res.charAt(17) ,test.Q18[0],test.Q18[1],test.Q18[2],test.Q18[3])?"1":"0";
+        answ+=control(res.charAt(18) ,test.Q19[0],test.Q19[1],test.Q19[2],test.Q19[3])?"1":"0";
+        answ+=control(res.charAt(19) ,test.Q20[0],test.Q20[1],test.Q20[2],test.Q20[3])?"1":"0";
+        answ+=control(res.charAt(20) ,test.Q21[0],test.Q21[1],test.Q21[2],test.Q21[3])?"1":"0";
+        answ+=control(res.charAt(21) ,test.Q22[0],test.Q22[1],test.Q22[2],test.Q22[3])?"1":"0";
+        answ+=control(res.charAt(22) ,test.Q23[0],test.Q23[1],test.Q23[2],test.Q23[3])?"1":"0";
+        answ+=control(res.charAt(23) ,test.Q24[0],test.Q24[1],test.Q24[2],test.Q24[3])?"1":"0";
+        answ+=control(res.charAt(24) ,test.Q25[0],test.Q25[1],test.Q25[2],test.Q25[3])?"1":"0";
+        answ+=control(res.charAt(25) ,test.Q26[0],test.Q26[1],test.Q26[2],test.Q26[3])?"1":"0";
+        answ+=control(res.charAt(26) ,test.Q27[0],test.Q27[1],test.Q27[2],test.Q27[3])?"1":"0";
+
+        showResult();
+    }
+}
+
+function showResult()   {
+    var nok=0;
+    var i,s;
+
+    for (i=0; i<answ.length;i++) {nok+=answ.charAt(i)=="1"?1:0;}
+    if(nok==27) s="ОТЛИЧНО";
+    if(nok<27) s="ХОРОШО";
+    if(nok<20.25) s="УДОВЛЕТВОРИТЕЛЬНО";
+    if (nok<13.5) s="НЕУДОВЛЕТВОРИТЕЛЬНО";
+    document.test.s1.
+        value="Количество правильных ответов "+nok+". Ваша оценка "+s+". Посмотрите на окно рядом с номером вопроса. Если ответ правильный, там (+). Если ответ ошибочен, там (-).";
+
+    with(document.test)
+    {
+        if (answ.charAt(0)=="1") {T1.value=" + "} else {T1.value=" - "};
+        if (answ.charAt(1)=="1") {T2.value=" + "} else {T2.value=" - "};
+        if (answ.charAt(2)=="1") {T3.value=" + "} else {T3.value=" - "};
+        if (answ.charAt(3)=="1") {T4.value=" + "} else {T4.value=" - "};
+        if (answ.charAt(4)=="1") {T5.value=" + "} else {T5.value=" - "};
+        if (answ.charAt(5)=="1") {T6.value=" + "} else {T6.value=" - "};
+        if (answ.charAt(6)=="1") {T7.value=" + "} else {T7.value=" - "};
+        if (answ.charAt(7)=="1") {T8.value=" + "} else {T8.value=" - "};
+        if (answ.charAt(8)=="1") {T9.value=" + "} else {T9.value=" - "};
+        if (answ.charAt(9)=="1") {T10.value=" + "} else {T10.value=" - "};
+        if (answ.charAt(10)=="1") {T11.value=" + "} else {T11.value=" - "};
+        if (answ.charAt(11)=="1") {T12.value=" + "} else {T12.value=" - "};
+        if (answ.charAt(12)=="1") {T13.value=" + "} else {T13.value=" - "};
+        if (answ.charAt(13)=="1") {T14.value=" + "} else {T14.value=" - "};
+        if (answ.charAt(14)=="1") {T15.value=" + "} else {T15.value=" - "};
+        if (answ.charAt(15)=="1") {T16.value=" + "} else {T16.value=" - "};
+        if (answ.charAt(16)=="1") {T17.value=" + "} else {T17.value=" - "};
+        if (answ.charAt(17)=="1") {T18.value=" + "} else {T18.value=" - "};
+        if (answ.charAt(18)=="1") {T19.value=" + "} else {T19.value=" - "};
+        if (answ.charAt(19)=="1") {T20.value=" + "} else {T20.value=" - "};
+        if (answ.charAt(20)=="1") {T21.value=" + "} else {T21.value=" - "};
+        if (answ.charAt(21)=="1") {T22.value=" + "} else {T22.value=" - "};
+        if (answ.charAt(22)=="1") {T23.value=" + "} else {T23.value=" - "};
+        if (answ.charAt(23)=="1") {T24.value=" + "} else {T24.value=" - "};
+        if (answ.charAt(24)=="1") {T25.value=" + "} else {T25.value=" - "};
+        if (answ.charAt(25)=="1") {T26.value=" + "} else {T26.value=" - "};
+        if (answ.charAt(26)=="1") {T27.value=" + "} else {T27.value=" - "};
+    }
+}
+function showhide(obj){
+    if(obj == 'none') return 'inline';
+    else return 'none';
+}
